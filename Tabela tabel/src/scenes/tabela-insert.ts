9019f3f04f2b26d6805d570b3ead3448c38f5815
Vanja Stojanović <vanja.stojanovic25@gmail.com>
2023-03-01 21:06:05 +0100

    Initialization of tabel insert scene

--- Tabela tabel/src/scenes/tabela-insert.ts
+++ Tabela tabel/src/scenes/tabela-insert.ts
@@ -1,2 +1,16 @@
-import { makeScene2D } from "@motion-canvas/2d";
+import { makeScene2D } from '@motion-canvas/2d/lib/scenes';
+import { Circle } from '@motion-canvas/2d/lib/components';
+import { createRef } from '@motion-canvas/core/lib/utils';
+import { all } from '@motion-canvas/core/lib/flow';
 
+export default makeScene2D(function* (view) {
+  const circle = createRef<Circle>();
+  view.add(<Circle ref={circle} width={100} height={100} />);
+
+  circle().fill('red');
+  yield;
+  circle().fill('blue');
+  yield;
+  circle().fill('red');
+  yield;
+});
